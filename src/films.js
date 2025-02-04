020f7d9bc5a1d2af6fb94df2cb588c888b56c535
DaniloEspinosa <115655795+DaniloEspinosa@users.noreply.github.com>
2025-02-04 22:26:53 +0100

    Commit final agregando los comentarios

--- src/films.js
+++ src/films.js
@@ -2,25 +2,25 @@ const movies = require('./data');
 
 // Exercise 1: Get the array of all directors.
 function getAllDirectors(array) {
-  let result = array.map((item) => item.director);
+  let result = array.map((item) => item.director); // Nuevo array con solo los directores utilizando map
   console.log('EXERCICE 1 ->', result);
   return result;
 }
 
 // Exercise 2: Get the films of a certain director
 function getMoviesFromDirector(array, director) {
-  let result = array.filter((item) => item.director === director);
+  let result = array.filter((item) => item.director === director); //Metodo filter para filtrar por director
   return result;
 }
 
 // Exercise 3: Calculate the average of the films of a given director.
 function moviesAverageOfDirector(array, director) {
-  let list = array.filter((item) => item.director === director);
+  let list = array.filter((item) => item.director === director); // Obtener las peliculas segun el director
 
   let result =
-    list.reduce((acc, item) => {
+    list.reduce((acc, item) => {                   //Utilizar el reduce para sumar los score
       return acc + item.score;
-    }, 0) / list.length;
+    }, 0) / list.length;                           //Dividir por la cantidad de items y obener el promedio
   return result;
 }
 
@@ -38,23 +38,23 @@ function orderByYear(array) {
   const result = array
     .sort((a, b) => {
       if (a.year > b.year) return 1;
-      if (a.year < b.year) return -1;
-      if (a.title.toLowerCase() > b.title.toLowerCase()) return 1;
+      if (a.year < b.year) return -1;           
+      if (a.title.toLowerCase() > b.title.toLowerCase()) return 1;  // En caso que el año sea el mismo ordenar por titulo
       if (a.title.toLowerCase() < b.title.toLowerCase()) return -1;
     })
-    .map((item) => item);
+    .map((item) => item);  // Hacer un map para devolver un nuevo array 
   return result;
 }
 
 // Exercise 6: Calculate the average of the movies in a category
 function moviesAverageByCategory(array, category) {
-  if (typeof category == 'number') return 0;
-  if (category == '') return 0;
-  const genre = category[0].toUpperCase() + category.slice(1).toLowerCase();
-  const arrayCategory = array.filter((item) => item.genre.includes(`${genre}`));
+  if (typeof category == 'number') return 0;                // Si se escribe un numero en la categoria retornar 0
+  if (category == '') return 0;                             // Si se escribe un string vacio en la categoria retornar 0
+  const genre = category[0].toUpperCase() + category.slice(1).toLowerCase();   //Dar el formato correcto a la categoria
+  const arrayCategory = array.filter((item) => item.genre.includes(`${genre}`));  //filtrar las peliculas por la categoria indicada
   const result =
-    arrayCategory.reduce((acc, item) => acc + item.score, 0) /
-    arrayCategory.length;
+    arrayCategory.reduce((acc, item) => acc + item.score, 0) /        //Utilizar el reduce para sumar los score
+    arrayCategory.length;                                             //Dividir por la cantidad de items y obener el promedio
   return parseFloat(result.toFixed(2));
 }
 
@@ -72,8 +72,8 @@ function hoursToMinutes(array) {
 
 // Exercise 8: Get the best film of a year
 function bestFilmOfYear(array, year) {
-  let result = array.sort((a, b) => b.score - a.score);
-  return [result.find((movie) => movie.year === year)];
+  let result = array.sort((a, b) => b.score - a.score);              // Ordenar todas las peliculas por el score
+  return [result.find((movie) => movie.year === year)];              // Una vez ordenadas obtener el valor mas alto por el año y devolver un array
 }
 
 // The following is required to make unit tests work.
